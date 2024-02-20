19a222ed85cbbe5380d8d931872eee083bf1f024
Alexander Chan <alyxchan87@gmail.com>
2024-02-20 06:56:22 -0800

    Add dap config for C and gdb

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,22 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function(_, opts)
+    local dap = require("dap")
+    dap.adapters.gdb = {
+      type = "executable",
+      command = "gdb",
+      args = { "-i", "dap" },
+    }
+
+    dap.configurations.c = {
+      name = "Launch",
+      type = "gdb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopAtBeginningOfMainSubprogram = false,
+    }
+  end,
+}
