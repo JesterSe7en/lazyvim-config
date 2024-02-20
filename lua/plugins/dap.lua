10a19252c19e4c8ffe0b4b1e39ab1b7f63712d84
Alex Chan <alyxchan87@gmail.com>
2024-02-20 10:47:31 -0800

    DAP configs

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,22 +1,39 @@
 return {
-  "mfussenegger/nvim-dap",
-  config = function(_, opts)
-    local dap = require("dap")
-    dap.adapters.gdb = {
-      type = "executable",
-      command = "gdb",
-      args = { "-i", "dap" },
-    }
+  {
+    "ldelossa/nvim-dap-projects",
+    config = function()
+      require("nvim-dap-projects").search_project_config()
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      vim.fn.sign_define("DapStopped", { text = "", texthl = "DiagnosticWarn" })
+      vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticInfo" })
+      vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DiagnosticError" })
+      vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DiagnosticInfo" })
+      vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "DiagnosticInfo" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = "mason.nvim",
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
 
-    -- dap.configurations.c = {
-    --   name = "Launch",
-    --   type = "gdb",
-    --   request = "launch",
-    --   program = function()
-    --     return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    --   end,
-    --   cwd = "${workspaceFolder}",
-    --   stopAtBeginningOfMainSubprogram = false,
-    -- }
-  end,
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        "cppdbg",
+        -- Update this to ensure that you have the debuggers for the langs you want
+      },
+    },
+  },
 }
